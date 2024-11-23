3c28c870d4872a6e7d34d74828342835b87d41c9
rai.peladas <draipeladas@gmail.com>
2024-11-23 15:18:03 +0800

    Move Player folder scripts in Gameplay folder, Add PlayerStats script

---
+++ Siomeow Legends/Assets/Scripts/Gameplay/Player/PlayerStats.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int health = 100; 
+    public int defense = 50; 
+    public int stamina = 80; 
+    public int mana = 0; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Siomeow Legends/Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int health = 100; 
+    [SerializeField] private int defense = 50; 
+    [SerializeField] private int stamina = 80; 
+    [SerializeField] private int mana = 0; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
