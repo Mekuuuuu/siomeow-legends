6372cafdb81feb25e2c667b762353c3aeaa095f6
rai.peladas <draipeladas@gmail.com>
2024-12-03 20:57:50 +0800

    Add special attack logic

--- Siomeow Legends/Assets/Scripts/Player/PlayerAttack.cs
+++ Siomeow Legends/Assets/Scripts/Player/PlayerAttack.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     private GameObject attackArea = default;
+    private GameObject specialAttackArea = default;
+
     private bool isAttacking = false;
-    private float attackDuration = 0.25f;
+    private float attackDuration = 0.35f;
     private float attackTimer = 0f;
+
+    private bool isSpecialAttacking = false;
+    private float specialAttackDuration = 0.65f;
+    private float specialAttackCooldown = 5f;
+    private float specialAttackTimer = 0f;
 
     public Animator anim;
     
@@ -13,29 +20,47 @@ public class PlayerAttack : MonoBehaviour
     void Start()
     {
         attackArea = transform.GetChild(0).gameObject;
+        specialAttackArea = transform.GetChild(1).gameObject;
+
+        attackArea.SetActive(isAttacking);
+        specialAttackArea.SetActive(isSpecialAttacking);
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckAttackTimer();
+        CheckSpecialAttackCooldown();
 
         if (Input.GetKeyDown(KeyCode.J))
         {
             Attack();
+        }
+        else if (Input.GetKeyDown(KeyCode.K) && !isSpecialAttacking && specialAttackTimer <= 0)
+        {
+            SpecialAttack();
         }
     }
 
     private void Attack()
     {
         if (!isAttacking)
-        {
-            attackArea.SetActive(isAttacking);
+        {            
             isAttacking = true; 
-
+            attackArea.SetActive(isAttacking);
             Animate();
         }
         
+    }
+
+    private void SpecialAttack()
+    {
+        if (!isSpecialAttacking)
+        {
+            isSpecialAttacking = true;
+            specialAttackArea.SetActive(isSpecialAttacking);
+            Animate();
+        }
     }
 
     private void CheckAttackTimer()
@@ -54,11 +79,40 @@ public class PlayerAttack : MonoBehaviour
         }
     }
 
+    private void CheckSpecialAttackCooldown()
+    {
+        if (isSpecialAttacking)
+        {
+            specialAttackTimer += Time.deltaTime;
+
+            if (specialAttackTimer >= specialAttackDuration)
+            {
+                isSpecialAttacking = false;
+                specialAttackArea.SetActive(isSpecialAttacking);
+                anim.SetBool("Special", isSpecialAttacking);
+            }
+        }
+
+        // Reset the cooldown timer when cooldown is complete
+        if (specialAttackTimer > 0 && specialAttackTimer < specialAttackCooldown)
+        {
+            specialAttackTimer += Time.deltaTime;
+        }
+        else if (specialAttackTimer >= specialAttackCooldown)
+        {
+            specialAttackTimer = 0; // Cooldown complete
+        }
+    }
+
     private void Animate()
     {
         if(isAttacking)
         {
             anim.SetBool("Attack", isAttacking); 
+        }
+        else if(isSpecialAttacking)
+        {
+            anim.SetBool("Special", isSpecialAttacking); 
         }
     }
 }
