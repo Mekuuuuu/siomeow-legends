5b0accc8c4697c0152bc43c62c7fac82d9c699b9
Gee Nine <69750024+lkpnchl@users.noreply.github.com>
2024-12-03 18:30:20 +0800

    Update Powerup Pickup scripts

---
+++ Siomeow Legends/Assets/Scripts/Player/PickupItem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    public enum PowerUp { Berserk, Movement, Stamina, Heal, Shield }
+    public PowerUp type;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerMovement player = other.GetComponent<PlayerMovement>();
+            if (player == null)
+            {
+                Debug.LogWarning("PlayerMovement component not found on the player.");
+                return;
+            }
+            PowerUpsHandler powerUpsHandler = player.GetComponent<PowerUpsHandler>();
+            powerUpsHandler.Initialize(player);
+            powerUpsHandler.ApplyPowerUp(type, 15f, 5f);
+
+            Destroy(gameObject);
+        }
+    }
+}
