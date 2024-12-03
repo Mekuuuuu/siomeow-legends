858a32ccef4b2c2df8f656ba54f1244c884366ec
rai.peladas <draipeladas@gmail.com>
2024-12-03 20:57:22 +0800

    Modified area size

--- Siomeow Legends/Assets/Scripts/Player/AttackArea.cs
+++ Siomeow Legends/Assets/Scripts/Player/AttackArea.cs
@@ -9,11 +9,11 @@ public class AttackArea : MonoBehaviour
         if(collider.GetComponent<PlayerStats>() != null)
         {
             PlayerStats health = collider.GetComponent<PlayerStats>();
-            
+
             int damage = Random.Range(minDamage, maxDamage + 1); // Add 1 to include maxDamage in the range
             health.TakeDamage(damage);
 
-            Debug.Log($"Damage: {damage}");
+            Debug.Log($"Normal Damage: {damage}");
         }
     }
 }
